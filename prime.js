9a561394d478ef3a21994ad7d810abd936ee5ddb
codewithrupak <rupakdash999@gmail.com>
2022-11-03 23:24:22 +0530

    Push prime code

---
+++ prime.js
@@ -0,0 +1,13 @@
+function numcheck(num){
+    let count = 0;
+    for(let x = 0; x <= num; x++){
+        if (num % x == 0){
+            
+        }
+    }if (count == 2){
+        console.log("is a prime number")
+    }else{
+        console.log("not a prime number")
+    }
+}numcheck(15)
+// just check line number 5 and add count
